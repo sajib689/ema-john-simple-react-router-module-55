5cfd809d9ae8a98774274fb716dff9ea81ba17d5
sajib689 <jibonraja11@gmail.com>
2023-08-08 17:11:01 +0600

    login

--- src/PrivateRoute/PrivateRoute.jsx
+++ src/PrivateRoute/PrivateRoute.jsx
@@ -1,15 +1,17 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../Providers/AuthProviders';
+import { useLocation } from 'react-router-dom';
 
 const PrivateRoute = ({children}) => {
     const {user,loading} = useContext(AuthContext)
+    const location = useLocation()
     if(loading) {
         return <div className='flex justify-center items-center'><span className="align-middle loading loading-ring loading-lg"></span></div>
     }
     if(user) {
         return children
     }
-    return <Navigate to='/login' replace={true}></Navigate>
+    return <Navigate to='/login' state={{from: location}} replace={true}></Navigate>
 };
 
 export default PrivateRoute;
